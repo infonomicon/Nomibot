91fac3814def0b83dca505318d03910687f6a740
Jared Kazimir <jaredkazimir@gmail.com>
2014-08-19 15:49:08 +0000

    add simple ping

---
+++ src/Ping.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Infonomicon\IrcBot;
+
+use Phergie\Irc\Bot\React\AbstractPlugin;
+use Phergie\Irc\Plugin\React\Command\CommandEventInterface as Event;
+use Phergie\Irc\Bot\React\EventQueueInterface as Queue;
+
+/**
+ * Ping plugin
+ */
+class Ping extends AbstractPlugin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return ['command.ping' => 'handle'];
+    }
+
+    /**
+     * Send a pong
+     *
+     * @param Event $event
+     * @param Queue $queue
+     */
+    public function handle(Event $event, Queue $queue)
+    {
+        $queue->ircPrivmsg($event->getSource(), "pong");
+    }
+}

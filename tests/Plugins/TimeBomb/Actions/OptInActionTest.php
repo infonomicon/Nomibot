13993d2c48a2bd27071777cdd018483f533d2c29
jkazimir <jaredkazimir@gmail.com>
2015-02-07 13:50:29 -0500

    test opt in action

---
+++ tests/Plugins/TimeBomb/Actions/OptInActionTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Nomibot\Plugins\TimeBomb\Actions;
+
+use Mockery as m;
+
+class OptInActionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvokeOptedOut()
+    {
+        $event = m::mock('Phergie\Irc\Plugin\React\Command\CommandEventInterface');
+        $queue = m::mock('Phergie\Irc\Bot\React\EventQueueInterface');
+        $game = m::mock('Nomibot\Plugins\TimeBomb\Game');
+        $optouts = m::mock('Nomibot\Plugins\TimeBomb\OptOutManager');
+
+        $game
+            ->shouldReceive('getOptOuts')
+            ->andReturn($optouts);
+
+        $event
+            ->shouldReceive('getNick')
+            ->andReturn('OptedOut');
+        $event
+            ->shouldReceive('getSource')
+            ->andReturn('channel');
+
+        $optouts
+            ->shouldReceive('contains')
+            ->with('OptedOut')
+            ->andReturn(true);
+        $optouts
+            ->shouldReceive('remove')
+            ->with('OptedOut');
+
+        $queue
+            ->shouldReceive('ircPrivmsg')
+            ->withArgs(['channel', "OptedOut: You've opted back in to the timebomb game."]);
+
+        $action = new OptInAction($event, $queue, $game);
+
+        $action();
+    }
+
+    public function testInvokeOptedIn()
+    {
+        $event = m::mock('Phergie\Irc\Plugin\React\Command\CommandEventInterface');
+        $queue = m::mock('Phergie\Irc\Bot\React\EventQueueInterface');
+        $game = m::mock('Nomibot\Plugins\TimeBomb\Game');
+        $optouts = m::mock('Nomibot\Plugins\TimeBomb\OptOutManager');
+
+        $game
+            ->shouldReceive('getOptOuts')
+            ->andReturn($optouts);
+
+        $event
+            ->shouldReceive('getNick')
+            ->andReturn('OptedOut');
+        $event
+            ->shouldReceive('getSource')
+            ->andReturn('channel');
+
+        $optouts
+            ->shouldReceive('contains')
+            ->with('OptedOut')
+            ->andReturn(false);
+
+        $queue
+            ->shouldReceive('ircPrivmsg')
+            ->withArgs(['channel', "OptedOut: You're already opted in."]);
+
+        $action = new OptInAction($event, $queue, $game);
+
+        $action();
+    }
+}
